17eb49914a44c82462ee7df7e6afe326ad558b6b
Ian García <iangarciag@outlook.com>
2022-08-27 03:17:04 -0500

    [WEB] Fixed linter formating

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
+        jest: true
     },
     extends: [
         'plugin:react/recommended',
-        'standard-with-typescript'
+        'standard-with-typescript',
+        'eslint:recommended',
+        "plugin:prettier/recommended"
     ],
     overrides: [],
     parserOptions: {
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App (): JSX.Element {
+function App(): JSX.Element {
   return (
-          <div className="App">
-              <header className="App-header">
-                  <p>
-                      Edit <code>src/App.tsx</code> and save to reload.
-                  </p>
-                  <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                  >
-                      Learn React
-                  </a>
-              </header>
-          </div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
