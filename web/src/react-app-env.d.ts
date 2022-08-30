b7eb8e83b44ae6e3586a08e3b3c6d8632161c152
Ian García <iangarciag@outlook.com>
2022-08-29 19:48:29 -0500

    [WEB] Added .env file

--- web/src/react-app-env.d.ts
+++ web/src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /* / <reference types="react-scripts" /> */
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      REACT_APP_API_URL: string;
+    }
+  }
+}
+
+declare module "babel-plugin-relay/macro" {
+  export { graphql as default } from "react-relay";
+}
