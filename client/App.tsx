3d2d9bb9bef66663149ff4607fd6af426bfc2eee
Ian García <iangarciag@outlook.com>
2022-08-30 18:30:30 -0500

    [INFRA] Removed unused .expo folder

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className="">
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
