2727f139bc7745001d1e717d6aa2d3aa3572da7b
Ian García <iangarciag@outlook.com>
2022-08-31 01:09:05 -0500

    [CLIENT] Implemented ESLINT & Prettier

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,14 @@
-import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView, Text, View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
-    return (
-        <SafeAreaView className="bg-gray-50 dark:bg-gray-900 h-screen">
-            <View className="items-center p-10 bg-cyan-100 dark:bg-cyan-800">
-                <Text className="font-black text-3xl">Open up App.js to start working on your app!</Text>
-            </View>
-            <StatusBar style="auto"/>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView className="bg-gray-50 dark:bg-gray-900 h-screen">
+      <View className="items-center p-10 bg-cyan-100 dark:bg-cyan-800">
+        <Text className="font-black text-3xl">Open up App.js to start working on your app!</Text>
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
 }
