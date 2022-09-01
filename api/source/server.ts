e00d04e57f8227a56ce85fb80881bbd57dca42cb
Pantro Huerta <pantro12@live.com.mx>
2022-09-01 18:31:19 -0500

    [API] fixed issue with dotenv port

--- api/source/server.ts
+++ api/source/server.ts
@@ -1,8 +1,9 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
+import { resolve } from "path";
 
 const dotenv = require("dotenv");
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: resolve(__dirname, "../.env") });
 
 process.on("uncaughtException", (error: Error) => {
   console.error("[!][API] UNCAUGHT_EXCEPTION :: SHUTTING DOWN!");
@@ -12,7 +13,7 @@ process.on("uncaughtException", (error: Error) => {
 });
 
 const application = require("./app");
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 9090;
 
 const server = application.listen(PORT, () => {
   console.log(`🚀 [API] Listening on port ${PORT}`);
