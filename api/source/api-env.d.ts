4a1e012793d68f982ebe5a0c855d630d2bf19755
Ian García <iangarciag@outlook.com>
2022-09-01 16:28:05 -0500

    [API] Added env types.

---
+++ api/source/api-env.d.ts
@@ -0,0 +1,12 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: string;
+      BASE_URL: string;
+      PG_CONNECTION_STRING: string;
+      PORT: number;
+    }
+  }
+}
