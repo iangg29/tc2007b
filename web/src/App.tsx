d66095caa2b040451bccecddb6f2a0ad779ec95c
Ian García <iangarciag@outlook.com>
2022-09-07 01:17:40 -0500

    [WEB] Implemented initial routes.
    Authentication within the /app/ prefix will be implemented in the future.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,9 @@ import React, { lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const Landing = lazy(async () => await import("./pages/Landing"));
+const Login = lazy(async () => await import("./pages/auth/Login"));
+const SignUp = lazy(async () => await import("./pages/auth/SignUp"));
+const Layout = lazy(async () => await import("./containers/Layout"));
 
 function App(): JSX.Element {
   return (
@@ -9,6 +12,9 @@ function App(): JSX.Element {
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/app/*" element={<Layout />} />
         </Routes>
       </Router>
     </>
