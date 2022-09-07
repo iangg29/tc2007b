b89384de9c8f03c24695f39a76ac7f63eb05073c
Ian García <iangarciag@outlook.com>
2022-09-07 01:21:26 -0500

    [WEB] Added 404 error page.

---
+++ web/src/pages/Error404.tsx
@@ -0,0 +1,11 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+const Error404 = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Error 404</h1>
+    </div>
+  );
+};
+
+export default Error404;
