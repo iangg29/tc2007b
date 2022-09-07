b83e86d4d4f745e26caa3a2604f33ccc8232855d
Ian García <iangarciag@outlook.com>
2022-09-07 01:21:53 -0500

    [WEB] Refactored Dashboard & Landing pages.

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,25 +1,6 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { useLazyLoadQuery } from "react-relay";
-import graphql from "babel-plugin-relay/macro";
-import { LandingQuery, LandingQuery$data } from "./__generated__/LandingQuery.graphql";
-
 const Landing = (): JSX.Element => {
-  const data: LandingQuery$data = useLazyLoadQuery<LandingQuery>(
-    graphql`
-      query LandingQuery {
-        users {
-          first_lastname
-          second_lastname
-          email
-        }
-      }
-    `,
-    {},
-  );
-
-  console.log(data);
-
   return <div>Landing page</div>;
 };
 
