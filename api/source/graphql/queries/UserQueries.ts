a51057364e11bc16381971160743cd2638433277
Ian García <iangarciag@outlook.com>
2022-09-07 00:04:21 -0500

    [API][GRAPHQL] Implemented initial User queries.

---
+++ api/source/graphql/queries/UserQueries.ts
@@ -0,0 +1,15 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import { GraphQLList } from "graphql";
+import { UserType } from "../../models/UserModel";
+import { db } from "../../database/database";
+import { USER_TABLE_NAME } from "../../database/utils/database_constants";
+
+export default {
+  users: {
+    type: GraphQLList(UserType),
+    resolve: () => {
+      return db.select().table(USER_TABLE_NAME);
+    },
+  },
+};
