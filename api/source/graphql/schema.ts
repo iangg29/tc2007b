bf5155ae42ddcfa72f4ba0ba54fa38bbfb1f1772
Ian García <iangarciag@outlook.com>
2022-09-07 00:05:54 -0500

    [API][GRAPHQL] Added graphql schema generator.

--- api/source/graphql/schema.ts
+++ api/source/graphql/schema.ts
@@ -1,38 +1,36 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
-import { buildSchema, GraphQLSchema } from "graphql";
+
+import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import UserQueries from "./queries/UserQueries";
+import RoleQueries from "./queries/RoleQueries";
+import RoleMutations from "./mutations/RoleMutations";
 
 /**
- * Generates application schema.
- * @todo Pull each model type by using different files.
+ * GraphQLObject that unifies every query that the application uses.
  */
-export const getSchema = (): GraphQLSchema => {
-  return buildSchema(`
-  type User {
-      id: ID!,
-      role_id: ID!,
-      name: String!,
-      first_lastname: String!,
-      second_lastname: String!,
-      password: String!,
-      cellphone: String!,
-      email: String!,
-      status: Int!,
-      active: Boolean!
-      created_at: String!,
-      updated_at: String!
-    }
-    
-   type Role {
-    id: ID!,
-    name: String!,
-    description: String!,
-    created_at: String!,
-    updated_at: String!
-   }
-    
-  type Query {
-    users: [User]
-    roles: [Role]
-  }
-`);
-};
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  description: "Application's GraphQL root query.",
+  fields: {
+    ...UserQueries,
+    ...RoleQueries,
+  },
+});
+
+/**
+ * GraphQLObject that unifies every mutation that the application uses.
+ */
+const RootMutation = new GraphQLObjectType({
+  name: "RootMutation",
+  description: "Application's GraphQL root mutations",
+  fields: {
+    ...RoleMutations,
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery,
+  mutation: RootMutation,
+});
+
+export default schema;
