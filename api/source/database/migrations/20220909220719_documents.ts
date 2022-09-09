9dc3b44c1254f6c0f6d60c71eb762ec355cb1ef5
Ian García <iangarciag@outlook.com>
2022-09-09 17:59:54 -0500

    [API] [DB] New documents table.

---
+++ api/source/database/migrations/20220909220719_documents.ts
@@ -0,0 +1,27 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import { Knex } from "knex";
+import { DOCUMENT_TABLE_NAME, DOCUMENT_TYPE_TABLE_NAME, USER_TABLE_NAME } from "../utils/database_constants";
+import CreateTableBuilder = Knex.CreateTableBuilder;
+
+export async function up(knex: Knex): Promise<void> {
+  if (!(await knex.schema.hasTable(DOCUMENT_TABLE_NAME))) {
+    await knex.schema.createTable(DOCUMENT_TABLE_NAME, (table: CreateTableBuilder) => {
+      // COLUMNS
+      table.string("id").primary().unique();
+      table.string("user_id").notNullable();
+      table.string("file_name").notNullable();
+      table.string("file_type_id").notNullable();
+      table.string("url").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+      // RELATIONSHIPS
+      table.foreign("user_id").references(`${USER_TABLE_NAME}.id`).deferrable("deferred");
+      table.foreign("file_type_id").references(`${DOCUMENT_TYPE_TABLE_NAME}.id`).deferrable("deferred");
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.dropTable(DOCUMENT_TABLE_NAME);
+}
