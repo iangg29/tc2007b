7310467ef636fd53c97dc92dec9d4379e0700c8d
Ian García <iangarciag@outlook.com>
2022-09-09 18:03:01 -0500

    [API] [DB] New FAQs table.

---
+++ api/source/database/migrations/20220909224201_faqs.ts
@@ -0,0 +1,21 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import { Knex } from "knex";
+import { FAQ_TABLE_NAME } from "../utils/database_constants";
+import CreateTableBuilder = Knex.CreateTableBuilder;
+
+export async function up(knex: Knex): Promise<void> {
+  if (!(await knex.schema.hasTable(FAQ_TABLE_NAME))) {
+    await knex.schema.createTable(FAQ_TABLE_NAME, (table: CreateTableBuilder) => {
+      table.string("id").primary().unique();
+      table.string("question").notNullable();
+      table.text("answer").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.dropTable(FAQ_TABLE_NAME);
+}
