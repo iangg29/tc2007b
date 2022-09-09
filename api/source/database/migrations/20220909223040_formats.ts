2f37b32c966d15b4bc766832e26bad114742a8a3
Ian García <iangarciag@outlook.com>
2022-09-09 18:01:19 -0500

    [API] [DB] New formats table.

---
+++ api/source/database/migrations/20220909223040_formats.ts
@@ -0,0 +1,26 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import { Knex } from "knex";
+import { CITATION_TABLE_NAME, DOCUMENT_TABLE_NAME, FORMAT_TABLE_NAME } from "../utils/database_constants";
+import CreateTableBuilder = Knex.CreateTableBuilder;
+
+export async function up(knex: Knex): Promise<void> {
+  if (!(await knex.schema.hasTable(FORMAT_TABLE_NAME))) {
+    await knex.schema.createTable(FORMAT_TABLE_NAME, (table: CreateTableBuilder) => {
+      // COLUMNS
+      table.string("id").primary().unique();
+      table.string("name");
+      table.string("citation_id").notNullable();
+      table.string("document_id").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+      // RELATIONSHIPS
+      table.foreign("citation_id").references(`${CITATION_TABLE_NAME}.id`).deferrable("deferred");
+      table.foreign("document_id").references(`${DOCUMENT_TABLE_NAME}.id`).deferrable("deferred");
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.dropTable(FORMAT_TABLE_NAME);
+}
