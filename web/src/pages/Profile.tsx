8095c499b16ff5b4f532f4f367986288e54f95de
Fermin Mendez <A01703366@itesm.mx>
2022-09-10 10:18:02 -0500

    Add profile page

---
+++ web/src/pages/Profile.tsx
@@ -0,0 +1,13 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import ProfileInfo from "../components/ProfileInfo";
+
+const Profile = (): JSX.Element => {
+  return (
+    <div>
+      <ProfileInfo />
+    </div>
+  );
+};
+
+export default Profile;
--- web/src/routes/index.ts
+++ web/src/routes/index.ts
@@ -4,11 +4,16 @@ import { iRoute } from "../shared/types/AppTypes";
 import { lazy } from "react";
 
 const Dashboard = lazy(async () => await import("../pages/Dashboard"));
+const Profile = lazy(async () => await import("../pages/Profile"));
 
 const routes: iRoute[] = [
   {
     path: "/",
     components: Dashboard,
+  },
+  {
+    path: "/profile",
+    components: Profile,
   },
 ];
 
