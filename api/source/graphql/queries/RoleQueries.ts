c04e773804ca56a594e785618de7e8462133f8b5
Ian García <iangarciag@outlook.com>
2022-09-13 00:40:39 -0500

    [API] Minor file changes.

--- api/source/graphql/queries/RoleQueries.ts
+++ api/source/graphql/queries/RoleQueries.ts
@@ -1,7 +1,7 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
 import { GraphQLList } from "graphql";
-import { RoleType } from "../../models/RoleModel";
+import { RoleType } from "../../types/RoleType";
 import { db } from "../../database/database";
 import { ROLE_TABLE_NAME } from "../../database/utils/database_constants";
 
