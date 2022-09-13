f2b3eeb2b131f251e34f620981e5fb481887992f
Ian García <iangarciag@outlook.com>
2022-09-13 15:17:47 -0500

    [WEB] [RELAY] Major changes to relay environment generator.

--- web/src/relay/RelayEnvironment.ts
+++ web/src/relay/RelayEnvironment.ts
@@ -1,21 +1,51 @@
-// (c) Tecnologico de Monterrey 2022, rights reserved.
-import { Environment, FetchFunction, Network, RecordSource, Store } from "relay-runtime";
-import genFetchQuery from "./FetchQuery";
+import {
+  CacheConfig,
+  Environment,
+  GraphQLResponse,
+  Network,
+  RecordSource,
+  RequestParameters,
+  Store,
+  Variables,
+} from "relay-runtime";
+import fetchGraphQL from "./FetchQuery";
+import Cookies from "js-cookie";
 
-/**
- * Make a request to the APIs GraphQL endpoint.
- * @param params Request parameters.
- * @param variables Request variables.
- */
-const fetchRelay = async (params: { name: string; text: string }, variables: any): Promise<any> => {
-  console.debug(`[DEBUG] [GRAPHQL] Fetching query ${params.name} with variables: ${JSON.stringify(variables)}`);
-  return await genFetchQuery(params.text, variables);
+const _DEV_ = process.env.NODE_ENV !== "production";
+
+const fetchFunction = async (
+  request: RequestParameters,
+  variables: Variables,
+  cacheConfig: CacheConfig,
+): Promise<GraphQLResponse> => {
+  const args = { request, variables, cacheConfig };
+  const token = Cookies.get("token");
+  return await fetchGraphQL({ ...args, token });
 };
 
-/**
- * Creates a new relay environment.
- */
-export default new Environment({
-  network: Network.create(fetchRelay as unknown as FetchFunction),
-  store: new Store(new RecordSource()),
-});
+export type RelayEnvironmentProps = Environment;
+
+class RelayEnvironment {
+  config = {
+    network: Network.create(fetchFunction),
+    store: new Store(new RecordSource()),
+  };
+
+  environment: RelayEnvironmentProps = new Environment(this.config);
+
+  constructor() {
+    this.resetEnvironment();
+  }
+
+  getEnvironment(): RelayEnvironmentProps {
+    return this.environment;
+  }
+
+  resetEnvironment(): RelayEnvironmentProps {
+    if (_DEV_) console.debug("[RELAY] Environment instance initialized.");
+    this.environment = new Environment(this.config);
+    return this.environment;
+  }
+}
+
+export default new RelayEnvironment();
