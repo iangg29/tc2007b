cee2d8b07900f574a1f31e65824ec62c25258cc6
Ian García <iangarciag@outlook.com>
2022-09-13 00:42:10 -0500

    [API] Initial authentication mutations.

---
+++ api/source/graphql/mutations/AuthMutations.ts
@@ -0,0 +1,65 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import { LoginResponseType } from "../../types/AuthTypes";
+import { GraphQLBoolean, GraphQLError, GraphQLNonNull, GraphQLString } from "graphql";
+import { ROLE_TABLE_NAME, USER_TABLE_NAME } from "../../database/utils/database_constants";
+import { db } from "../../database/database";
+
+export default {
+  login: {
+    type: LoginResponseType,
+    args: {
+      email: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      password: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+    },
+    resolve: async (_: any, { email, password }: any) => {
+      const users = await db.select().from(USER_TABLE_NAME).where("email", email);
+      if (users.length <= 0) throw new GraphQLError("The credentials do not match any of our records.");
+      const user = users[0];
+      const role = await db.select().from(ROLE_TABLE_NAME).where("id", user.role_id);
+
+      return {
+        success: true,
+        error: null,
+        user: {
+          ...user,
+          role: role[0],
+        },
+      };
+    },
+  },
+  register: {
+    type: GraphQLBoolean,
+    args: {
+      name: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      first_lastname: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      second_lastname: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      cellphone: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      email: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      password: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      confirm_password: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+    },
+    resolve: (_: any, args: any) => {
+      const { name, first_lastname, second_lastname, cellphone, email, password, confirm_password } = args;
+      return true;
+    },
+  },
+};
