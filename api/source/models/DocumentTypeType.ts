cc63d959e64c1892d0e7adb16df63c0db6f0e767
alfredohuerta <eagarciahuerta@hotmail.com>
2022-09-17 00:21:44 -0500

    Created type for document type

---
+++ api/source/models/DocumentTypeType.ts
@@ -0,0 +1,24 @@
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+
+export const DocumentTypeType = new GraphQLObjectType({
+  name: "Document Type",
+  description: "Type of document uploaded",
+  fields: {
+    id: {
+      type: GraphQLNonNull(GraphQLID),
+      description: "Document Type ID",
+    },
+    name: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Document Type name",
+    },
+    created_at: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Document Type creation date",
+    },
+    updated_at: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Last time Document Type was updated",
+    }
+  },
+});
