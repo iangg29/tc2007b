004470a577a9f64476678842429accfa0e8aafa8
alfredohuerta <eagarciahuerta@hotmail.com>
2022-09-16 21:42:12 -0500

    [API] Added application type to graphql/models

---
+++ api/source/models/ApplicationType.ts
@@ -0,0 +1,60 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { UserType } from "./UserModel";
+import { ApplicationStatusType } from "./ApplicationStatusType";
+
+export const ApplicationType = new GraphQLObjectType({
+  name: "Application",
+  description: "Main application model, contains all the information related with the applications of the users into the system",
+  fields: {
+    id: {
+      type: GraphQLNonNull(GraphQLID),
+      description: "Application ID",
+    },
+    user: {
+      type: UserType,
+      description: "User that uploaded the request",
+    },
+    title: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Application title",
+    },
+    deadline: {
+      type: GraphQLString,
+      description: "",
+    },
+    start_time: {
+      type: GraphQLString,
+      description: "",
+    },
+    end_time: {
+      type: GraphQLString,
+      description: "",
+    },
+    emission_date: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Application emission date",
+    },
+    response_date: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Application response date",
+    },
+    applicationStatus: {
+      type: ApplicationStatusType,
+      description: "Application status",
+    },
+    /*citation_id: {
+      type: ,
+      description: "",
+    },*/
+    created_at: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Application creation date",
+    },
+    updated_at: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Last time application was updated",
+    },
+  },
+});
