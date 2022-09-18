6eb6f950999c5782166e08d3cdcd49ea6a205744
Fermin Mendez <A01703366@itesm.mx>
2022-09-18 15:19:01 -0500

    [WEB] Add Interfaces Profile and json Prop

---
+++ web/src/components_profile/ProfileLabel.tsx
@@ -0,0 +1,19 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+interface props {
+  field: string;
+  value: any;
+}
+
+const ProfileLabel = ({ field, value }: props): JSX.Element => {
+  return (
+    <div className=" w-full px-3 mb-6">
+      <label className="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2">{field}</label>
+      <p className="appearance-none block w-2/3 bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-2 px-4 leading-tight focus:outline-none  focus:border-gray-500 text-xl">
+        {value}
+      </p>
+    </div>
+  );
+};
+
+export default ProfileLabel;
---
+++ web/src/components_profile/ProfileTypes.d.ts
@@ -0,0 +1,11 @@
+export interface profileInfo {
+  name: String;
+  first_lastname: String;
+  second_lastname: String;
+  cellphone: String;
+  email: String;
+  status: Number;
+  }
+
+
+  export interface fieldValue { field: string; value: any }
