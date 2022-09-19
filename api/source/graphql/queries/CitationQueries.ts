3fec767e720f601a1b7d424150b49ed19dc3529a
alfredohuerta <eagarciahuerta@hotmail.com>
2022-09-19 15:46:51 -0500

    Changed citation to citations

--- api/source/graphql/queries/CitationQueries.ts
+++ api/source/graphql/queries/CitationQueries.ts
@@ -4,7 +4,7 @@ import { db } from "../../database/database";
 import { CITATION_TABLE_NAME } from "../../database/utils/database_constants";
 
 export default {
-  citation: {
+  citations: {
     type: GraphQLList(CitationType),
     resolve: () => {
       return db.select().table(CITATION_TABLE_NAME);
