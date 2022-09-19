6033dbe136c11cdfefc5a7f5b3043fec8829f5bc
Ian García <iangarciag@outlook.com>
2022-09-19 15:53:47 -0500

    [API] Updated Role mutation resolver.

--- api/source/graphql/mutations/RoleMutations.ts
+++ api/source/graphql/mutations/RoleMutations.ts
@@ -1,7 +1,7 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
 import { RoleType } from "../../types/RoleType";
-import { GraphQLError, GraphQLNonNull, GraphQLString } from "graphql";
+import { GraphQLBoolean, GraphQLError, GraphQLNonNull, GraphQLString } from "graphql";
 import { v4 as uuid } from "uuid";
 import { db } from "../../database/database";
 import { ROLE_TABLE_NAME } from "../../database/utils/database_constants";
@@ -16,14 +16,18 @@ export default {
       description: {
         type: GraphQLNonNull(GraphQLString),
       },
+      is_default: {
+        type: GraphQLBoolean,
+      },
     },
-    resolve: async (_: any, { name, description }: any) => {
+    resolve: async (_: any, { name, description, is_default }: any) => {
       const id = uuid();
       await db(ROLE_TABLE_NAME)
         .insert({
           id,
-          name,
+          name: name.toUpperCase(),
           description,
+          default: is_default ?? false,
         })
         .catch((error: Error) => {
           console.error(error);
