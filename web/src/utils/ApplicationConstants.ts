02aafe9a03f6cc7391e4ed46c95f974ba36359a7
Ian García <iangarciag@outlook.com>
2022-09-19 15:58:21 -0500

    [WEB] Implemented new app constants.

---
+++ web/src/utils/ApplicationConstants.ts
@@ -0,0 +1,4 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+export const APP_NAME = "Secretaría de la Cultura";
+export const APP_NAME_SHORT = "Secretaría de la Cultura del Estado de Querétaro";
