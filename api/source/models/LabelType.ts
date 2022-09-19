23e177b056c96080807dce9939755a90776aabe0
alfredohuerta <eagarciahuerta@hotmail.com>
2022-09-19 09:49:43 -0500

    [API] Created LabelType file

---
+++ api/source/models/LabelType.ts
@@ -0,0 +1,28 @@
+import { GraphQLObjectType, GraphQLID, GraphQLNonNull, GraphQLString } from "graphql";
+
+export const LabelType = new GraphQLObjectType({
+  name: "Label",
+  description: "Label assigned to each application",
+  fields: {
+    id: {
+      type: GraphQLNonNull(GraphQLID),
+      description: "Label's ID",
+    },
+    name: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Label's name",
+    },
+    description: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Label's description",
+    },
+    created_at: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Label's creation date",
+    },
+    updated_at: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Last time label was updated",
+    },
+  },
+});
