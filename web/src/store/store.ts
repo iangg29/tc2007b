859c02b0b96bf421c95601c31ba9aff0f9668e18
Ian García <iangarciag@outlook.com>
2022-09-20 19:54:26 -0500

    [WEB] [REDUX] Configured store.

---
+++ web/src/store/store.ts
@@ -0,0 +1,12 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
