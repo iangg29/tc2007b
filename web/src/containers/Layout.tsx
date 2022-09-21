5b92186d7b7180128ac7efad197427d5a534a860
Ian García <iangarciag@outlook.com>
2022-09-20 19:53:22 -0500

    [WEB] Added isLoggedIn check.

--- web/src/containers/Layout.tsx
+++ web/src/containers/Layout.tsx
@@ -1,14 +1,27 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+import { lazy, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Wrapper from "./Wrapper";
 import { iRoute } from "../shared/types/AppTypes";
 import routes from "../routes";
+import { useAppSelector } from "../store/hooks";
+import { selectIsLoggedIn } from "../store/slices/authSlice";
 
 const Error404 = lazy(async () => await import("../pages/Error404"));
 
 const Layout = (): JSX.Element => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn === null) return;
+    if (isLoggedIn === false) {
+      // TODO: Try pulling user data from TOKEN (possibly stored in Cookies)
+      navigate("/login");
+    }
+  }, [isLoggedIn]);
+
   return (
     <div>
       <Wrapper>
