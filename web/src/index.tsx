315ec50ec6e5c8ef07783fa3661a85f223d47428
Ian García <iangarciag@outlook.com>
2022-09-20 19:53:40 -0500

    [WEB] Wrapped main app within redux provider.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,15 +4,19 @@ import "./assets/css/main.css";
 import { RelayEnvironmentProvider } from "react-relay";
 import relayEnvironment from "./relay/RelayEnvironment";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <RelayEnvironmentProvider environment={relayEnvironment.getEnvironment()}>
-    <StrictMode>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <App />
-      </Suspense>
-    </StrictMode>
+    <Provider store={store}>
+      <StrictMode>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <App />
+        </Suspense>
+      </StrictMode>
+    </Provider>
   </RelayEnvironmentProvider>,
 );
