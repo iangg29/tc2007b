8f6e414969021e9adb3efd2e36b4f1f9daba25b9
Ian García <iangarciag@outlook.com>
2022-09-22 19:12:54 -0500

    [WEB] Dashboard testing.

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,7 +1,26 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
+import graphql from "babel-plugin-relay/macro";
+import { useLazyLoadQuery } from "react-relay";
+import { DashboardQuery, DashboardQuery$data } from "./__generated__/DashboardQuery.graphql";
+
 const Dashboard = (): JSX.Element => {
-  return <div></div>;
+  const data: DashboardQuery$data = useLazyLoadQuery<DashboardQuery>(
+    graphql`
+      query DashboardQuery {
+        users {
+          id
+          name
+          email
+        }
+      }
+    `,
+    {},
+  );
+
+  console.debug(data);
+
+  return <div>Test</div>;
 };
 
 export default Dashboard;
