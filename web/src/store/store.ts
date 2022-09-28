3ac620de1cd1328d9cb87e4a4287e8e52396f474
Ian García <iangarciag@outlook.com>
2022-09-27 19:30:10 -0500

    [WEB] [AUTH] Implemented redux persist.

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -1,12 +1,25 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import authReducer from "./slices/authSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
