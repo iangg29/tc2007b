5c6d4086826e7952297e57e98d43cf76208e0a32
Arisbeth Aguirre <a01274803@itesm.mx>
2022-09-28 02:48:48 -0500

    [WEB] Modified DocumentList

--- web/src/components/DocumentList/CheckboxDocument.tsx
+++ web/src/components/DocumentList/CheckboxDocument.tsx
@@ -1,6 +1,6 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import React from "react";
+import { useRef } from "react";
 
 interface params {
   id: string;
@@ -8,15 +8,24 @@ interface params {
 }
 
 const CheckboxDocument = ({ name, id }: params): JSX.Element => {
+  const myRef = useRef<HTMLInputElement | null>(null);
+
+  function test(): void {
+    console.log(myRef?.current?.checked);
+    console.log(myRef?.current?.id);
+  }
+
   return (
     <>
       <div className="flex items-center mt-3">
         <input
+          ref={myRef}
           id={id}
           type="checkbox"
           className="w-4 h-4 text-blue-600 bg-gray-100 rounded
               border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2
               dark:bg-gray-700 dark:border-gray-600"
+          onClick={() => test()}
         ></input>
         <label htmlFor="default-checkbox" className="ml-2 text- font-medium text-gray-900 dark:text-gray-300">
           {name}
@@ -25,5 +34,7 @@ const CheckboxDocument = ({ name, id }: params): JSX.Element => {
     </>
   );
 };
+
+console.log();
 
 export default CheckboxDocument;
