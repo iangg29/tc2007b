8beb251602eabc0e19d3331dcb39c61235961431
Ian García <iangarciag@outlook.com>
2022-09-27 19:30:25 -0500

    [WEB] [AUTH] Wrapped app with PersistGate.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -7,18 +7,21 @@ import { RelayEnvironmentProvider } from "react-relay";
 import relayEnvironment from "./relay/RelayEnvironment";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <RelayEnvironmentProvider environment={relayEnvironment.getEnvironment()}>
     <Provider store={store}>
-      <StrictMode>
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <App />
-        </Suspense>
-      </StrictMode>
+      <PersistGate persistor={persistor} loading={<h1>Loading...</h1>}>
+        <StrictMode>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <App />
+          </Suspense>
+        </StrictMode>
+      </PersistGate>
     </Provider>
   </RelayEnvironmentProvider>,
 );
