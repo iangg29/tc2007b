c6fbbc98752a82ce72762a4a560952762cbdd6e0
Ian García <iangarciag@outlook.com>
2022-09-27 19:09:21 -0500

    [API] [AUTH] Fixed auth check.
    Redirect to login page if null.

--- web/src/containers/Layout.tsx
+++ web/src/containers/Layout.tsx
@@ -7,6 +7,8 @@ import { iRoute } from "../shared/types/AppTypes";
 import routes from "../routes";
 import { useAppSelector } from "../store/hooks";
 import { selectIsLoggedIn } from "../store/slices/authSlice";
+import Cookies from "js-cookie";
+import Login from "../pages/auth/Login";
 
 const Error404 = lazy(async () => await import("../pages/Error404"));
 
@@ -15,22 +17,30 @@ const Layout = (): JSX.Element => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (isLoggedIn === false) {
+    if ((isLoggedIn !== undefined && isLoggedIn === false) || Cookies.get("token") === undefined) {
       navigate("/login");
     }
-  }, [isLoggedIn]);
+  });
+
+  if (isLoggedIn === true) {
+    return (
+      <>
+        <Wrapper>
+          <Routes>
+            {routes.map((route: iRoute, idx: number) => (
+              <Route key={idx} path={route.path} element={<route.components />} />
+            ))}
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </Wrapper>
+      </>
+    );
+  }
 
   return (
-    <div>
-      <Wrapper>
-        <Routes>
-          {routes.map((route: iRoute, idx: number) => (
-            <Route key={idx} path={route.path} element={<route.components />} />
-          ))}
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-      </Wrapper>
-    </div>
+    <>
+      <Login />
+    </>
   );
 };
 export default Layout;
