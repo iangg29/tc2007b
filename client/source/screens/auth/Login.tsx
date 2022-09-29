58ed7504fb55135fcedcea43e3b16d0eb16cdb1d
Ian García <iangarciag@outlook.com>
2022-09-29 14:41:36 -0500

    [CLIENT] Added login post request.
    Missing redirect.

--- client/source/screens/auth/Login.tsx
+++ client/source/screens/auth/Login.tsx
@@ -2,21 +2,46 @@
 
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
-import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { selectIsLoggedIn, setIsLoggedIn, setToken, setUser } from "../../store/slices/authSlice";
+import axios, { AxiosResponse } from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_URL } from "@env";
 
 const Login = (): JSX.Element => {
+  const isLoggedIn: boolean = useAppSelector<boolean>(selectIsLoggedIn);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  const dispatch = useAppDispatch();
 
   const navigation = useNavigation();
 
   const login = () => {
+    (async () => {
+      await axios
+        .post(`${API_URL}/auth/login`, { email, password })
+        .then((res: AxiosResponse<any>) => {
+          const { status, token, user } = res.data;
+          if (status === "success") {
+            AsyncStorage.setItem("token", `Bearer ${token}`);
+            dispatch(setUser(user));
+            dispatch(setToken(token));
+            dispatch(setIsLoggedIn(true));
+          } else {
+            Alert.alert("Ooops!", "Something went wrong.");
+          }
+        })
+        .catch((error) => console.error(error));
+    })();
     console.debug("LOGIN :: ATTEMPT :: BTN_CLICK");
   };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-1 items-center justify-center space-y-10 px-10">
+        {isLoggedIn ? <Text>You are logged in.</Text> : <Text>You are not logged in</Text>}
         <Text className="font-bold text-2xl">Secretaria de Cultura</Text>
         <TouchableOpacity onPress={() => navigation.navigate("Landing")}>
           <Text>Go Landing</Text>
