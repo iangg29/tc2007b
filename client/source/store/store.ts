e8373fc6bd3417e4cbb6e947cd11df1f5288ebd3
Ian García <iangarciag@outlook.com>
2022-09-29 12:39:18 -0500

    [CLIENT] Created redux store with persist configuration.

---
+++ client/source/store/store.ts
@@ -0,0 +1,26 @@
+// (c) Tecnologico de Monterrey 2022, rights reserved.
+
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
