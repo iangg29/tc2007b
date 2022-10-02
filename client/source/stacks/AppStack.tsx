5e30de036490884e9826c17ba5afde3b13d89241
Ian García <iangarciag@outlook.com>
2022-10-02 02:17:53 -0500

    [CLIENT] Added SignUp screen.

--- client/source/stacks/AppStack.tsx
+++ client/source/stacks/AppStack.tsx
@@ -2,6 +2,7 @@
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../screens/auth/Login";
+import SignUp from "../screens/auth/SignUp";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export const AppStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={SignUp} />
     </Stack.Navigator>
   );
 };
