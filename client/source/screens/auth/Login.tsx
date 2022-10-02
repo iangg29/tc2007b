563e93786d0eeb7c9501f2482d4fc5c7f415c1b7
Ian García <iangarciag@outlook.com>
2022-10-02 00:48:02 -0500

    [CLIENT] Cleaned up Login screen.

--- client/source/screens/auth/Login.tsx
+++ client/source/screens/auth/Login.tsx
@@ -1,22 +1,18 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { Alert, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { selectIsLoggedIn, setIsLoggedIn, setToken, setUser } from "../../store/slices/authSlice";
+import { useAppDispatch } from "../../store/hooks";
+import { setIsLoggedIn, setToken, setUser } from "../../store/slices/authSlice";
 import axios, { AxiosResponse } from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API_URL } from "@env";
 
 const Login = (): JSX.Element => {
-  const isLoggedIn: boolean = useAppSelector<boolean>(selectIsLoggedIn);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const dispatch = useAppDispatch();
-
-  const navigation = useNavigation();
 
   const login = () => {
     (async () => {
@@ -35,18 +31,12 @@ const Login = (): JSX.Element => {
         })
         .catch((error) => console.error(error));
     })();
-    console.debug("LOGIN :: ATTEMPT :: BTN_CLICK");
   };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-1 items-center justify-center space-y-10 px-10">
-        <Text>{API_URL}</Text>
-        {isLoggedIn ? <Text>You are logged in.</Text> : <Text>You are not logged in</Text>}
         <Text className="font-bold text-2xl">Secretaria de Cultura</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Landing")}>
-          <Text>Go Landing</Text>
-        </TouchableOpacity>
         <View className="space-y-5 text-left min-w-[90%]">
           <Text>Correo electronico</Text>
           <TextInput
