d8fb5d3b8c4425123f4949137df61684d7250e17
Ian García <iangarciag@outlook.com>
2022-10-02 00:48:16 -0500

    [CLIENT] Cleaned up Landing screen.

--- client/source/screens/general/Landing.tsx
+++ client/source/screens/general/Landing.tsx
@@ -1,23 +1,45 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { API_URL } from "@env";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { selectUser, setIsLoggedIn, setToken, setUser } from "../../store/slices/authSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Landing = () => {
   const navigation = useNavigation();
+  const dispatch = useAppDispatch();
+
+  const user: any = useAppSelector(selectUser);
+
+  const removeToken = async () => {
+    await AsyncStorage.removeItem("token");
+  };
+
+  const logout = () => {
+    try {
+      (async () => {
+        dispatch(setToken(""));
+        dispatch(setUser({ name: "", first_lastname: "", second_lastname: "", cellphone: "", email: "" }));
+        dispatch(setIsLoggedIn(false));
+        await removeToken();
+      })();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <View>
       <Text>This is the landing page</Text>
-      <View className="flex-row justify-between mx-10">
-        <Text>The env variable is</Text>
-        <Text>{`${API_URL}`}</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-          <Text>Login</Text>
-        </TouchableOpacity>
+      <View className="text-center mx-10 ">
+        <Text>Your user is </Text>
+        <Text>{`${user?.name}`}</Text>
       </View>
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
