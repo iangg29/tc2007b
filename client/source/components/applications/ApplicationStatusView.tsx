7b3adce12a5dd924c9d3be24de9ef381d3dd02b0
Fermin Mendez <A01703366@itesm.mx>
2022-10-02 21:08:08 -0500

    [Client] Include status message

--- client/source/components/applications/ApplicationStatusView.tsx
+++ client/source/components/applications/ApplicationStatusView.tsx
@@ -1,29 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, FlatList } from "react-native";
-import { graphql, useLazyLoadQuery } from "react-relay/hooks";
-import ApplicationCheckPoint from "./general/components/Application/ApplicationCheckPoint";
+import ApplicationCheckPoint from "./ApplicationCheckPoint";
 
 interface Props {
-  status: any;
+  status: string;
 }
 
-const ApplicationView = ({ status }: Props) => {
-  const [num, setNum] = useState(2);
+const ApplicationStatusView = ({ status }: Props) => {
+  const [num, setNum] = useState(0);
   const [myData, setMyData] = useState([]);
-  useEffect(() => {
-    if (status == "finalizado") {
-      setNum(2);
-    }
+  const [message, setMessage] = useState("");
 
+  const setStatus = (status: string) => {
+    switch (status) {
+      case "documentos enviados":
+        setNum(1);
+        setMessage(
+          "Tus documentación se ha enviado con éxito.\nEstamos revisando que correspondan con su descripción.",
+        );
+        break;
+      case "en correccion":
+        setNum(2);
+        setMessage("Algo salió mal con alguno de tus documentos. Te pedimos los vuelvas a subir.");
+        break;
+      case "documentos aceptados":
+        setNum(3);
+        setMessage("Tus documentos parecen en orden. Estamos evaluando tu solicitud.");
+        break;
+      case "rechazada":
+        setNum(4);
+        setMessage("Lo sentimos, tu solicitud ha sido rechazada.");
+        break;
+      case "aceptada":
+        setNum(5);
+        setMessage("Feliciades, tus solicitud a sido aceptada. Recuerda subir la evidencia para justificar el apoyo.");
+        break;
+      case "finalizada":
+        setNum(6);
+        setMessage("Hemos recibido tu evidencia.");
+        break;
+      default:
+        setNum(-1);
+    }
+  };
+  const createData = () => {
     setMyData([
       {
         label: "Documentos enviados",
         isOn: num >= 1,
-        line: true,
-      },
-      {
-        label: "Documentos Revisados",
-        isOn: num >= 2,
         line: true,
       },
       {
@@ -32,26 +56,55 @@ const ApplicationView = ({ status }: Props) => {
         line: true,
       },
       {
-        label: "Solicitud aprobada",
+        label: "Resultado de la solicitud",
         isOn: num >= 4,
+        line: true,
+      },
+      {
+        label: "Evidencia del apoyo enviada",
+        isOn: num == 6,
+        line: true,
+        disable: num == 4,
+      },
+      {
+        label: "Solicitud terminada",
+        isOn: num == 4 || num == 6,
         line: false,
       },
     ]);
-  }, []);
+  };
+
+  useEffect(() => {
+    setStatus(status);
+    createData();
+  }, [num]);
 
   return (
-    <View className="flex flex-col pl-4 justify-start">
-      <Text className=" text-4xl text-indigo-500 font-semibold py-4">Estatus</Text>
+    <View>
+      <View className="flex flex-col pl-4 justify-start">
+        <Text className=" text-4xl text-indigo-500 font-semibold py-4">
+          Status: {status}
+          Num: {num}
+        </Text>
 
-      <FlatList
-        data={myData}
-        renderItem={({ item }) => (
-          <ApplicationCheckPoint isTurnOn={item.isOn} labelText={item.label} isLine={item.line}></ApplicationCheckPoint>
-        )}
-        keyExtractor={(item) => item.label}
-      />
+        <FlatList
+          data={myData}
+          renderItem={({ item }) => (
+            <ApplicationCheckPoint
+              isTurnOn={item.isOn}
+              labelText={item.label}
+              isLine={item.line}
+              disable={item.disable}></ApplicationCheckPoint>
+          )}
+          keyExtractor={(item) => item.label}
+        />
+      </View>
+
+      <View className="bg-blue-100  shadow-lg mx-auto rounded-xl m-6 ">
+        <Text className="font-semibold text-indigo-500 text-lg m-4 ">{message}</Text>
+      </View>
     </View>
   );
 };
 
-export default ApplicationView;
+export default ApplicationStatusView;
