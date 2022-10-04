f12647518720cd9937c1210918ecfce06f51c6f1
Arisbeth Aguirre <a01274803@itesm.mx>
2022-10-04 14:51:45 -0500

    [API] Added Citation Update Mutation

--- api/source/graphql/mutations/CitationMutations.ts
+++ api/source/graphql/mutations/CitationMutations.ts
@@ -4,7 +4,7 @@ import { CitationType } from "../../types/CitationType";
 import { GraphQLError, GraphQLID, GraphQLList, GraphQLNonNull, GraphQLString } from "graphql";
 import { v4 as uuid } from "uuid";
 import { db } from "../../database/database";
-import { CITATION_DOCUMENTS_TABLE_NAME, CITATION_TABLE_NAME } from "../../database/utils/database_constants";
+import { CITATION_DOCUMENTS_TABLE_NAME, CITATION_TABLE_NAME, DOCUMENT_TYPE_TABLE_NAME } from "../../database/utils/database_constants";
 
 export default {
   createCitation: {
@@ -20,30 +20,34 @@ export default {
         type: GraphQLNonNull(GraphQLString),
       },
       document_types: {
-        type: GraphQLList(GraphQLID)
-      }
+        type: GraphQLList(GraphQLID),
+      },
     },
     resolve: async (_: any, { title, description, end_date, document_types }: any) => {
       const id = uuid();
 
-      await db.transaction(async (trx) => {
-        await trx(CITATION_TABLE_NAME).insert({
-          id,
-          title,
-          description,
-          end_date,
-        });
+      await db
+        .transaction(async (trx) => {
+          await trx(CITATION_TABLE_NAME).insert({
+            id,
+            title,
+            description,
+            end_date,
+          });
 
-        console.log(document_types);
+          console.log(document_types);
 
-        await Promise.all (document_types.map(async (element: any) => (
-          await trx(CITATION_DOCUMENTS_TABLE_NAME).insert({
-            citation_id: id,
-            document_type_id: element,
-          }))
-        ));
-
-      }).catch((error: Error) => {
+          await Promise.all(
+            document_types.map(
+              async (element: any) =>
+                await trx(CITATION_DOCUMENTS_TABLE_NAME).insert({
+                  citation_id: id,
+                  document_type_id: element,
+                }),
+            ),
+          );
+        })
+        .catch((error: Error) => {
           console.error(error);
           throw new GraphQLError(error.name);
         });
@@ -53,6 +57,85 @@ export default {
       return {
         ...newCitation[0],
       };
+    },
+  },
+
+  updateCitation: {
+    type: CitationType,
+    args: {
+      id: {
+        type: GraphQLNonNull(GraphQLID),
+      },
+      title: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      description: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      end_date: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+      document_types: {
+        type: GraphQLList(GraphQLID),
+      },
+    },
+    resolve: async (_: any, { id, title, description, end_date, document_types }: any) => {
+      await db
+        .transaction(async (trx) => {
+          await trx(CITATION_TABLE_NAME).where({ id }).update({ title, description, end_date });
+
+          const citationDocuments = await trx.select().table(CITATION_DOCUMENTS_TABLE_NAME).where({ citation_id: id });
+
+          const listCitationDocuments = citationDocuments.map((element: any) => { const newElement: any = element.document_type_id; return newElement });
+          
+          //console.log(listCitationDocuments);
+
+          const docsToBeDeleted = listCitationDocuments.filter((element: any) => !document_types.includes(element)).map(
+            async (filteredElement: any) => {
+              const newElement: any = filteredElement;
+              return newElement;
+            }
+          );
+          
+
+          const docsToBeAdded = document_types.filter((element: any) => !listCitationDocuments.includes(element)).map(
+            async (filteredElement: any) => {
+              const newElement: any = filteredElement;
+              return newElement;
+            }
+          );
+
+          //console.log("Add ", docsToBeAdded);
+          //console.log("Delete ", docsToBeDeleted);
+
+
+          await Promise.all(
+            docsToBeAdded.map(
+              async (element: any) =>
+                await trx(CITATION_DOCUMENTS_TABLE_NAME).insert({
+                  citation_id: id,
+                  document_type_id: element,
+                }),
+            ),
+          );
+
+          await Promise.all(
+            docsToBeDeleted.map(
+              async (element: any) =>
+                await trx(CITATION_DOCUMENTS_TABLE_NAME).where({document_type_id: element}).delete()
+            ),
+          );
+
+          
+        })
+        .catch((error: Error) => {
+          console.error(error);
+          throw new GraphQLError(error.name);
+        });
+
+      const newCitation = await db.select().from(CITATION_TABLE_NAME).where({ id });
+
+      return newCitation[0];
     },
   },
 };
