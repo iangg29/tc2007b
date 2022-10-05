a7cee4ea73f9bddff10811a564d46b18c3722891
Fermin Mendez <A01703366@itesm.mx>
2022-10-05 17:41:50 -0500

    Resolve login merge confict

--- client/source/screens/auth/Login.tsx
+++ client/source/screens/auth/Login.tsx
@@ -2,55 +2,6 @@
 
 import { useLayoutEffect, useState } from "react";
 import { Alert, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
-<<<<<<< HEAD
-import { useMutation } from "react-relay";
-import { graphql } from "react-relay/hooks";
-
-// import { LoginMutation, LoginMutation$data } from "./__generated__/LoginMutation.graphql";
-
-const Login = (): JSX.Element => {
-  // const [email, setEmail] = useState<string>("");
-  // const [password, setPassword] = useState<string>("");
-
-  // const [sendLogin, isInFlight] = useMutation<LoginMutation>(graphql`
-  //   mutation LoginMutation($email: String!, $password: String!) {
-  //     login(email: $email, password: $password) {
-  //       success
-  //       error
-  //       user {
-  //         id
-  //         email
-  //       }
-  //     }
-  //   }
-  // `);
-
-  // const login = () => {
-  //   sendLogin({
-  //     variables: {
-  //       email,
-  //       password,
-  //     },
-  //     onCompleted: (data: LoginMutation$data) => {
-  //       // TODO: Modify LoginResponseType to return token.
-  //       // TODO: Set token asynchronously w/AsyncStorage.
-  //       // await AsyncStorage.setItem("token", "Bearer TOKEN");
-  //       // TODO: Set authenticated user in state w/Redux.
-  //       console.debug(data);
-  //       if (data.login.success) {
-  //         console.debug("LOGIN SUCCESSFUL");
-  //       } else {
-  //         console.error("LOGIN FAILED");
-  //         Alert.alert("Error", data.login.error ?? "No se ha podido iniciar sesión.", null, {
-  //           onDismiss: () => {
-  //             setPassword("");
-  //           },
-  //         });
-  //       }
-  //     },
-  //   });
-  // };
-=======
 import { useAppDispatch } from "../../store/hooks";
 import { setIsLoggedIn, setToken, setUser } from "../../store/slices/authSlice";
 import axios, { AxiosResponse } from "axios";
@@ -89,20 +40,13 @@ const Login = (): JSX.Element => {
         .catch((error) => console.error(error));
     })();
   };
->>>>>>> d083bb6d933358fd8ef406c75fd2f45645046c62
 
   return (
     <SafeAreaView className="dark flex-1 bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-50">
       <View className="flex-1 items-center justify-center space-y-10 px-10">
-<<<<<<< HEAD
-        <Text className="font-bold text-2xl">Secretaria de Cultura</Text>
-        {/* <View className="space-y-5 text-left min-w-[90%]">
-          <Text>Correo electronico</Text>
-=======
         <Text className="font-bold text-2xl text-gray-900 dark:text-white">Secretaria de Cultura</Text>
         <View className="space-y-5 text-left min-w-[90%]">
           <Text className="text-gray-900 dark:text-white">Correo electronico</Text>
->>>>>>> d083bb6d933358fd8ef406c75fd2f45645046c62
           <TextInput
             onChangeText={(text: string) => setEmail(text)}
             textContentType="emailAddress"
@@ -125,10 +69,6 @@ const Login = (): JSX.Element => {
           />
         </View>
         <TouchableOpacity className="rounded-lg bg-sky-800 px-8 py-3" onPress={login}>
-<<<<<<< HEAD
-          <Text className="font-semibold text-sky-50">{isInFlight ? "Loading..." : "Iniciar sesión"}</Text>
-        </TouchableOpacity> */}
-=======
           <Text className="font-semibold text-sky-50">Iniciar sesión</Text>
         </TouchableOpacity>
         <View>
@@ -136,7 +76,6 @@ const Login = (): JSX.Element => {
             <Text className="text-gray-700 dark:text-gray-200">Crear cuenta</Text>
           </TouchableOpacity>
         </View>
->>>>>>> d083bb6d933358fd8ef406c75fd2f45645046c62
       </View>
     </SafeAreaView>
   );
