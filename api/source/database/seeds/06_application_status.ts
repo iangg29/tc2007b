7bddc559da9dd8970c091c6ee24edff8063df651
Naiztu <a01707404@tec.mx>
2022-10-04 21:49:30 -0500

    [API] Aplication status seed

--- api/source/database/seeds/06_application_status.ts
+++ api/source/database/seeds/06_application_status.ts
@@ -1,13 +1,17 @@
 import { Knex } from "knex";
+import { APPLICATION_STATUS_TABLE_NAME } from "../utils/database_constants";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("table_name").del();
+    await knex(APPLICATION_STATUS_TABLE_NAME).del();
 
     // Inserts seed entries
-    await knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
+    await knex(APPLICATION_STATUS_TABLE_NAME).insert([
+        { id: "1", name:"En corrección", order:0 },
+        { id: "2", name:"Pendiente de revisión", order:1 },
+        { id: "3", name:"Documentos aceptados", order:2 },
+        { id: "4", name:"Rechazado", order:3 },
+        { id: "5", name:"Aceptado", order:4 },
+        { id: "6", name:"Finalizado", order:5 }
     ]);
 };
