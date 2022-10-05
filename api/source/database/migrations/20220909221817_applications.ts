feef24b6e12cd3b265605ccde821f38a4bc150de
Naiztu <a01707404@tec.mx>
2022-10-04 23:23:36 -0500

    [API] Add Seeds Applications

--- api/source/database/migrations/20220909221817_applications.ts
+++ api/source/database/migrations/20220909221817_applications.ts
@@ -19,11 +19,11 @@ export async function up(knex: Knex): Promise<void> {
       table.string("image").notNullable();
       table.string("description").notNullable();
       table.string("support").notNullable();
-      table.date("deadline").notNullable();
+      table.date("deadline");
       table.timestamp("start_time").defaultTo(knex.fn.now());
-      table.timestamp("end_time").notNullable();
-      table.date("emission_date").notNullable();
-      table.date("response_date").notNullable();
+      table.timestamp("end_time");
+      table.date("emission_date").defaultTo(knex.fn.now());
+      table.date("response_date");
       table.string("application_status_id").notNullable();
       table.string("citation_id").notNullable();
       table.timestamp("created_at").defaultTo(knex.fn.now());
--- api/source/database/seeds/07_applications.ts
+++ api/source/database/seeds/07_applications.ts
@@ -1,13 +1,47 @@
 import { Knex } from "knex";
+import { APPLICATION_TABLE_NAME } from "../utils/database_constants";
 
 export async function seed(knex: Knex): Promise<void> {
-    // Deletes ALL existing entries
-    await knex("table_name").del();
+  // Deletes ALL existing entries
+  await knex(APPLICATION_TABLE_NAME).del();
 
-    // Inserts seed entries
-    await knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
-    ]);
-};
+  // Inserts seed entries
+  await knex(APPLICATION_TABLE_NAME).insert([
+    {
+      id: "1",
+      user_id: "1",
+      title: "Obra de arte",
+      image: "https://www.artistasdelatierra.com/obras/foto104185.jpg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit sapien nisi, eget faucibus magna lobortis eu. Sed arcu nibh, varius eu finibus in, tincidunt nec tellus. Nullam suscipit mi libero, posuere sodales nunc congue quis. Cras rhoncus malesuada posuere. In sed velit at ex tincidunt sodales quis sit amet neque. Duis sodales libero at accumsan convallis. Curabitur feugiat justo a tellus tempor, at egestas ante mattis. Ut sagittis rhoncus iaculis.",
+      support:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit sapien nisi, eget faucibus magna lobortis eu. Sed arcu nibh, varius eu finibus in, tincidunt nec tellus. Nullam suscipit mi libero, posuere sodales nunc congue quis. Cras rhoncus malesuada posuere. In sed velit at ex tincidunt sodales quis sit amet neque. Duis sodales libero at accumsan convallis. Curabitur feugiat justo a tellus tempor, at egestas ante mattis. Ut sagittis rhoncus iaculis.",
+      application_status_id: "1",
+      citation_id: "1",
+    },
+    {
+      id: "2",
+      user_id: "2",
+      title: "Obra de arte2",
+      image: "https://www.artistasdelatierra.com/obras/foto104185.jpg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit sapien nisi, eget faucibus magna lobortis eu. Sed arcu nibh, varius eu finibus in, tincidunt nec tellus. Nullam suscipit mi libero, posuere sodales nunc congue quis. Cras rhoncus malesuada posuere. In sed velit at ex tincidunt sodales quis sit amet neque. Duis sodales libero at accumsan convallis. Curabitur feugiat justo a tellus tempor, at egestas ante mattis. Ut sagittis rhoncus iaculis.",
+      support:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit sapien nisi, eget faucibus magna lobortis eu. Sed arcu nibh, varius eu finibus in, tincidunt nec tellus. Nullam suscipit mi libero, posuere sodales nunc congue quis. Cras rhoncus malesuada posuere. In sed velit at ex tincidunt sodales quis sit amet neque. Duis sodales libero at accumsan convallis. Curabitur feugiat justo a tellus tempor, at egestas ante mattis. Ut sagittis rhoncus iaculis.",
+      application_status_id: "1",
+      citation_id: "1",
+    },
+    {
+      id: "3",
+      user_id: "3",
+      title: "Obra de arte3",
+      image: "https://www.artistasdelatierra.com/obras/foto104185.jpg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit sapien nisi, eget faucibus magna lobortis eu. Sed arcu nibh, varius eu finibus in, tincidunt nec tellus. Nullam suscipit mi libero, posuere sodales nunc congue quis. Cras rhoncus malesuada posuere. In sed velit at ex tincidunt sodales quis sit amet neque. Duis sodales libero at accumsan convallis. Curabitur feugiat justo a tellus tempor, at egestas ante mattis. Ut sagittis rhoncus iaculis.",
+      support:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras hendrerit sapien nisi, eget faucibus magna lobortis eu. Sed arcu nibh, varius eu finibus in, tincidunt nec tellus. Nullam suscipit mi libero, posuere sodales nunc congue quis. Cras rhoncus malesuada posuere. In sed velit at ex tincidunt sodales quis sit amet neque. Duis sodales libero at accumsan convallis. Curabitur feugiat justo a tellus tempor, at egestas ante mattis. Ut sagittis rhoncus iaculis.",
+      application_status_id: "1",
+      citation_id: "1",
+    },
+  ]);
+}
