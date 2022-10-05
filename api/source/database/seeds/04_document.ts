1c58e2c89e1ce494ae6fddf2b20d088543bb133f
Naiztu <a01707404@tec.mx>
2022-10-04 19:32:45 -0500

    [API] Seed document

--- api/source/database/seeds/04_document.ts
+++ api/source/database/seeds/04_document.ts
@@ -10,7 +10,21 @@ export async function seed(knex: Knex): Promise<void> {
     {
       id: "1",
       user_id: "1",
-      file_name: "example.pdf",
+      file_name: "con1.pdf",
+      file_type_id: "1",
+      url: "https://www.usebeq.edu.mx/PaginaWEB/Content/USICAMM/CE22-23/CONVOCATORIA%20PROMOCI%C3%93N%20HORIZONTAL%202022%20vf.pdf",
+    },
+    {
+      id: "4",
+      user_id: "1",
+      file_name: "con2.pdf",
+      file_type_id: "1",
+      url: "https://www.usebeq.edu.mx/PaginaWEB/Content/USICAMM/CE22-23/CONVOCATORIA%20PROMOCI%C3%93N%20HORIZONTAL%202022%20vf.pdf",
+    },
+    {
+      id: "5",
+      user_id: "1",
+      file_name: "con3.pdf",
       file_type_id: "1",
       url: "https://www.usebeq.edu.mx/PaginaWEB/Content/USICAMM/CE22-23/CONVOCATORIA%20PROMOCI%C3%93N%20HORIZONTAL%202022%20vf.pdf",
     },
