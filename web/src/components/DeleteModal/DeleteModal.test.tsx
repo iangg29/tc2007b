6233efe2172c42f2b22154df6ea4682c7c9c166d
JordanaBM <A01707434@itesm.mx>
2022-10-06 18:12:44 -0500

    [WEB] Modified jest test

--- web/src/components/DeleteModal/DeleteModal.test.tsx
+++ web/src/components/DeleteModal/DeleteModal.test.tsx
@@ -1,7 +1,7 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { render } from "@testing-library/react";
-import DeleteModal from "./DeleteModal";
+// import { render } from "@testing-library/react";
+// import DeleteModal from "./DeleteModal";
 
 // onClose incomplete function
 describe("DeleteModal tests", () => {
