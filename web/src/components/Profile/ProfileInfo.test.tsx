9943d4bd765129251e4936e5e99359413e6ae672
Fermin Mendez <A01703366@itesm.mx>
2022-10-07 01:15:18 -0500

    [WEB] Unused components in test

--- web/src/components/Profile/ProfileInfo.test.tsx
+++ web/src/components/Profile/ProfileInfo.test.tsx
@@ -1,5 +1,4 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
-import ProfileInfo from "./ProfileInfo";
 
 describe("Test ProfileInfo", () => {
   it("render component", () => {
--- web/src/components/Profile/ProfileLabel.test.tsx
+++ web/src/components/Profile/ProfileLabel.test.tsx
@@ -1,5 +1,4 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
-import ProfileLabel from "./ProfileLabel";
 
 describe("Test ProfileInfo", () => {
   it("render component", () => {
