a8344633f04171cfdb0811d00b1b63c59620e5be
JordanaBM <A01707434@itesm.mx>
2022-10-07 16:41:23 -0500

    [WEB] Fixed navBar test

--- web/src/components/EditModal/EditModal.test.tsx
+++ web/src/components/EditModal/EditModal.test.tsx
@@ -2,10 +2,12 @@
 
 // import { render } from "@testing-library/react";
 // import EditModal from "./EditModal";
+// import { MockPayloadGenerator } from 'relay-test-utils';
+// import { useRelayEnvironment } from 'react-relay/hooks';
 
 // Generates API requests and marks it as an error
 describe("EditModal tests", () => {
   it("renders EditModal", () => {
-    // render(<EditModal show={true} onClose={false}  header="Modal edición de convocatoria" citationId="1" />);
+    // render(<EditModal/>);
   });
 });
--- web/src/components/includes/navBar/NavBar.test.tsx
+++ web/src/components/includes/navBar/NavBar.test.tsx
@@ -1,22 +1,18 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import NavBar from "./NavBar";
 import { BrowserRouter as Router } from "react-router-dom";
 
-describe("Test Arrow Drop component", () => {
-  it("render component", () => {
-    render(
-      <Router>
-        <NavBar />
-      </Router>,
-    );
-  });
+beforeEach(() =>
+  render(
+    <Router>
+      <NavBar />
+    </Router>,
+  ),
+);
+
+test("NavBar home validation", () => {
+  const homeEl = screen.getByText(/home/i);
+  expect(homeEl).toBeInTheDocument();
 });
-
-// beforeEach(() => render(<NavBar />));
-
-// test("NavBar home validation", () => {
-//   const homeEl = screen.getByText(/home/i);
-//   expect(homeEl).toBeInTheDocument();
-// });
