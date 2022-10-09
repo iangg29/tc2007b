1e414fc5a0e9ffcffa7d1a6dbb11361ebcba5606
Fermin Mendez <A01703366@itesm.mx>
2022-10-09 15:47:24 -0500

    [CLIENT] Added Application Query

--- client/source/screens/applications/ApplicationsScreen.tsx
+++ client/source/screens/applications/ApplicationsScreen.tsx
@@ -4,29 +4,39 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text } from "react-native";
 import { graphql, useLazyLoadQuery } from "react-relay/hooks";
+import ApplicationList from "../../components/applications/ApplicationList";
 
-import ApplicationStatusView from "../../components/applications/ApplicationStatusView";
 import { ApplicationsScreenQuery, ApplicationsScreenQuery$data } from "./__generated__/ApplicationsScreenQuery.graphql";
 
+import { useAppSelector } from "../../store/hooks";
+import { selectUser } from "../../store/slices/authSlice";
+
 const ApplicationsScreen = (): JSX.Element => {
+  const user: any = useAppSelector(selectUser);
+  const user_id = user.id;
   const navigation = useNavigation();
-  const context_id = "bcb1f0b7-fbd1-4752-8199-2238e298957a";
-  const data_application_list: ApplicationsScreenQuery$data = useLazyLoadQuery<ApplicationsScreenQuery>(
+
+  const data: ApplicationsScreenQuery$data = useLazyLoadQuery<ApplicationsScreenQuery>(
     graphql`
-      query ApplicationsScreenQuery {
-        users {
+      query ApplicationsScreenQuery($user_id: ID!) {
+        applicationByUserID(user_id: $user_id) {
           id
+          title
+          applicationStatus {
+            name
+          }
         }
       }
     `,
-    {},
+    { user_id },
   );
+  const { applicationByUserID } = data;
 
   return (
     <View className=" pl-2">
       <Text className=" text-4xl text-indigo-500 font-semibold py-4">Mis solicitudes</Text>
       <View>
-        <ApplicationStatusView status="" title="title" />
+        <ApplicationList applicationByUserID={applicationByUserID}></ApplicationList>
       </View>
     </View>
   );
