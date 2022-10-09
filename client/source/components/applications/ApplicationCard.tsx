227564b790929fe0b29677c2b64d75f25e7d39ce
Fermin Mendez <A01703366@itesm.mx>
2022-10-09 15:37:52 -0500

    [CLIENT] Include estatus modal

--- client/source/components/applications/ApplicationCard.tsx
+++ client/source/components/applications/ApplicationCard.tsx
@@ -1,14 +1,34 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
-import { Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, TouchableOpacity, Modal } from "react-native";
+import ApplicationStatusView from "./ApplicationStatusView";
 
 interface Props {
-  id: string;
   title: string;
+  status: {
+    name;
+  };
 }
 
-const ApplicationCard = ({ id, title }: Props) => {
+const ApplicationCard = ({ title, status }: Props) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleStatus = () => {
+    setModalVisible(!modalVisible);
+    console.log("OPEN MODAL");
+  };
   return (
-    <View className="max-w mb-4 bg-white rounded-xl shadow-md overflow-hidden ">
+    <View className=" rounded-xl shadow-md overflow-hidden ">
+      <Modal
+        className="w-full h-full rounded-xl m-8 "
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <ApplicationStatusView status={status.name} title={title}></ApplicationStatusView>
+      </Modal>
       <View>
         <Image className="h-32 w-full object-cover" source={{ uri: "https://reactjs.org/logo-og.png" }} />
       </View>
@@ -16,7 +36,9 @@ const ApplicationCard = ({ id, title }: Props) => {
         <Text className=" tracking-wide text-lg text-indigo-500 font-semibold">Solicitud: {title}</Text>
         <View className="flex flex-row justify-between ">
           <Text className="text-lg mt-1 text-slate-500">Convocatoria</Text>
-          <Text className="text-lg mt-1 text-slate-500">Estado:</Text>
+          <TouchableOpacity onPress={handleStatus}>
+            <Text className="text-lg mt-1 text-slate-500">Dar seguimiento </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
