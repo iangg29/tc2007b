22ed265091ed965ac2ef0c675b76f0e74c249fe2
Fermin Mendez <A01703366@itesm.mx>
2022-10-09 15:56:19 -0500

    [CLIENT] Clean up citation list

--- client/source/components/citations/CitationList.tsx
+++ client/source/components/citations/CitationList.tsx
@@ -5,26 +5,22 @@ import { graphql, useLazyLoadQuery } from "react-relay/hooks";
 import CitationCard from "./CitationCard";
 
 const CitationList = () => {
-  const context_id = "bcb1f0b7-fbd1-4752-8199-2238e298957a";
-
-  //const data_Citation_list: CitationListQuery$data = useLazyLoadQuery<CitationListQuery>(
   const data_Citation_list: any = useLazyLoadQuery(
     graphql`
       query CitationListQuery {
-        users {
+        citations {
+          title
           id
         }
       }
     `,
     {},
   );
-  const mydata: any = data_Citation_list;
-
   return (
     <View>
       <Text>This is the Citation List page: </Text>
       <FlatList
-        data={mydata.applicationsByUserID}
+        data={data_Citation_list?.citations}
         renderItem={({ item }) => <CitationCard id={item.id} title={item.title} />}
         keyExtractor={(item) => item.id}
       />
