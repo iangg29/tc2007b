d5aaa267d506ace5f19f92ec961833d1b73d4688
alfredohuerta <eagarciahuerta@hotmail.com>
2022-10-09 20:40:47 -0500

    [WEB] Added filtering element

--- web/src/pages/suppRequests/Applications.tsx
+++ web/src/pages/suppRequests/Applications.tsx
@@ -3,8 +3,9 @@
 import { useLazyLoadQuery } from "react-relay";
 import graphql from "babel-plugin-relay/macro";
 import RequestMap from "../../components/RequestCard/RequestMap";
-
+import FilterByLabelsMap from "../../components/Filter/FilterByLabelsMap";
 import { ApplicationsQuery$data, ApplicationsQuery } from "./__generated__/ApplicationsQuery.graphql";
+import { useState } from "react";
 
 const Applications = (): JSX.Element => {
   const data: ApplicationsQuery$data = useLazyLoadQuery<ApplicationsQuery>(
@@ -33,19 +34,40 @@ const Applications = (): JSX.Element => {
     {},
     { fetchPolicy: "network-only" },
   );
-
   const { applications } = data;
   const empty = applications?.length === 0;
+  const [selected, setSelected] = useState("");
 
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleChange = (event: any) => {
+    setSelected(event.target.value);
+  };
   return (
     <>
       <h5 className="py-5 text-2xl text-main-100">Solicitudes enviadas</h5>
+      {empty ? (
+        <h1 className="col-span-3 text-center">
+          <br />
+          No hay solicitudes.
+        </h1>
+      ) : (
+        <select
+          id={"Etiquetas"}
+          className={
+            "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block max-w-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          }
+          value={selected}
+          onChange={handleChange}
+        >
+          <option selected={true}></option>
+          {applications?.map((element: any) => (
+            <FilterByLabelsMap element={element} key={element.labels.id}></FilterByLabelsMap>
+          ))}
+        </select>
+      )}
       <div className="grid grid-cols-3">
-        {empty ? (
-          <h1 className="col-span-3 text-center">
-            <br />
-            No hay solicitudes.
-          </h1>
+        {selected === "" ? (
+          <p>Show some</p>
         ) : (
           applications?.map((element: any) => (
             <RequestMap
