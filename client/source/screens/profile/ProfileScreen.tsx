ed55b405eb62e945b26cf53f8cf5739249b68036
JordanaBM <A01707434@itesm.mx>
2022-10-10 22:19:32 -0500

    [CLIENT][API] Add FAQs seed and view

--- client/source/screens/profile/ProfileScreen.tsx
+++ client/source/screens/profile/ProfileScreen.tsx
@@ -1,38 +1,15 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { Scope } from "@babel/traverse";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, Image, FlatList, TouchableOpacity, ScrollView } from "react-native";
 import { graphql, useLazyLoadQuery } from "react-relay/hooks";
+import { Logout } from "../../components/profile/Logout";
 import ProfileDocsList from "../../components/profile/ProfileDocsList";
-
-
-import UserDocs from "../../components/profile/UserDocs";
-import { useAppSelector, useAppDispatch } from "../../store/hooks";
-import { selectUser, setIsLoggedIn, setToken, setUser } from "../../store/slices/authSlice";
+import { useAppSelector} from "../../store/hooks";
+import { selectUser } from "../../store/slices/authSlice";
 import { ProfileScreenQuery, ProfileScreenQuery$data } from "./__generated__/ProfileScreenQuery.graphql";
 
 const ProfileScreen = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-
-  const removeToken = async () => {
-    await AsyncStorage.removeItem("token");
-  };
-
-  const logout = () => {
-    try {
-      (async () => {
-        dispatch(setToken(""));
-        dispatch(setUser({ id: "", name: "", first_lastname: "", second_lastname: "", cellphone: "", email: "" }));
-        dispatch(setIsLoggedIn(false));
-        await removeToken();
-      })();
-    } catch (e) {
-      console.error(e);
-    }
-  };
 
   const user: any = useAppSelector(selectUser);
 
@@ -61,10 +38,9 @@ const ProfileScreen = (): JSX.Element => {
     <ScrollView>
     <View className="flex-1 bg-white">
       <View>
-      <View className="flex items-end px-8">
-        <TouchableOpacity className="bg-blue-600 rounded-lg text-lg font-semibold px-3" onPress={logout}>
-          <Text className="text-white text-lg font-semibold px-3">Logout</Text>
-        </TouchableOpacity>
+      <View className="flex items-end px-8 py-2">
+        {/* Logout */}
+       <Logout/>
         </View>
         <View className="flex items-center">
           <Image
@@ -74,11 +50,11 @@ const ProfileScreen = (): JSX.Element => {
           />
           <Text className="text-blue-600 text-xl font-semibold py-2">{`${user?.name} ${user?.first_lastname} ${user?.second_lastname} `}</Text>
         </View>
-
         <View className="mx-16">
           <Text className="text-m font-semibold pt-2">Correo electrónico: </Text>
           <Text className="bg-blue-100 text-slate-600 rounded-lg text-lg font-semibold px-3">{`${user?.email}`} </Text>
         </View>
+        {/* List of user documents  */}
         <ProfileDocsList/>
       </View> 
     </View>
