95d1249abafd8499e9186abe0be3c2a8ceeffb61
alfredohuerta <eagarciahuerta@hotmail.com>
2022-10-13 17:45:35 -0500

    [WEB] Changed component to recieve only one label object at the time

--- web/src/components/Filter/FilterByLabels.tsx
+++ web/src/components/Filter/FilterByLabels.tsx
@@ -1,45 +1,19 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-/*
-import { useMemo } from "react";
-*/
-
-interface labels {
-  id: string;
-  name: String;
-}
-
 interface Props {
-  label: labels[];
+  label: {
+    id: string;
+    label_name: string;
+  };
 }
 
 const FilterByLabels = ({ label }: Props): JSX.Element => {
+  console.debug(label);
   return (
     <>
-      {label?.map((element: any) => (
-        <option value={element.id} key={element.id}>
-          {element.label_name}
-        </option>
-      ))}
-      {/* {label?.map((element: any) =>
-        useMemo(
-          () => (
-            <option value={element.id} key={element.id}>
-              {element.label_name}
-            </option>
-          ),
-          [element],
-        ),
-      )} */}
-      {/* {useMemo(
-        () =>
-          label?.map((element: any) => (
-            <option value={element.id} key={element.id}>
-              {element.label_name}
-            </option>
-          )),
-        [label],
-      )} */}
+      <option value={label.id} key={label.id}>
+        {label.label_name}
+      </option>
     </>
   );
 };
