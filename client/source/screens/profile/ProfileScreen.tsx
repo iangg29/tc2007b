6afc4d706f928a7b25d60bbe849d16bf6672262f
JordanaBM <A01707434@itesm.mx>
2022-10-13 15:06:50 -0500

    [CLIENT] Add updated for user Profile Picture

--- client/source/screens/profile/ProfileScreen.tsx
+++ client/source/screens/profile/ProfileScreen.tsx
@@ -1,9 +1,7 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
 import React from "react";
-import { View, Text, Image, ScrollView } from "react-native";
-import EditDocModal from "../../components/profile/EditDocModal";
-
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import { Logout } from "../../components/profile/Logout";
 import ProfileDocsList from "../../components/profile/ProfileDocsList";
 import UploadImage from "../../components/profile/UploadImage";
@@ -13,23 +11,16 @@ import { selectUser } from "../../store/slices/authSlice";
 const ProfileScreen = (): JSX.Element => {
   const user: any = useAppSelector(selectUser);
 
-  console.log(user?.profile_img)
-
   return (
-
-    <ScrollView className="flex-1 bg-white">
-      <View className="flex-1">
+    <SafeAreaView className="dark flex-1 text-gray-900 dark:bg-gray-900 dark:text-gray-50">
+      <ScrollView className="flex-1 bg-white">
+        <View className="flex-1">
           <View className="flex items-end px-8 py-2">
             {/* Logout */}
             <Logout />
           </View>
           <View className="flex items-center">
-            <Image
-              className="rounded-full"
-              style={{ width: 150, height: 150 }}
-              source={{uri: "https://dl.memuplay.com/new_market/img/com.vicman.newprofilepic.icon.2022-06-07-21-33-07.png"}}
-            />
-            <UploadImage/>
+            <UploadImage profileImg={user?.profile_img} />
             <Text className="text-blue-600 text-xl font-semibold py-2">{`${user?.name} ${user?.first_lastname} ${user?.second_lastname} `}</Text>
           </View>
           <View className="mx-16">
@@ -41,7 +32,8 @@ const ProfileScreen = (): JSX.Element => {
           {/* List of user documents  */}
           <ProfileDocsList />
         </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
