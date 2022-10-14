98a08a76edd91f3496844f8ec9f3bae490a73af2
Alejandro Martínez Luna <A01276785@tec.mx>
2022-10-14 13:57:23 -0500

    [API] [WEB] Change on Photos Routes

--- api/source/routes/photos.routes.ts
+++ api/source/routes/photos.routes.ts
@@ -1,43 +1,43 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
-
 import { Request, Response, Router } from "express";
 import path from "path";
-// const path = require("path");
+import { validateToken } from "../utils/authentication";
+const multer = require("multer");
+// const upload = multer();
+const storage = multer.diskStorage({
+  destination: function (req: any, file: any, cb: any) {
+    cb(null, "public/uploads/photos");
+  },
+  filename: function (req: any, file: any, cb: any) {
+    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+  },
+});
+
+const upload = multer({ storage }).any();
+// const route = path.resolve(__dirname, "../../public/uploads/files/", file.name);
 
 const router = Router();
 
-interface FileRequest extends Request {
-  files: any;
-}
-
-router.post("/upload/photo", async (req: Request, res: Response) => {
-  console.log({ req: (req as FileRequest).files });
-  if (!(req as FileRequest).files) {
-    return res.status(400).send("No files were uploaded.");
-  }
-
-  const file = (req as FileRequest).files.photo;
-  const file_extension = file.mimetype.split("/").pop();
-
-  switch (file_extension) {
-    case "jpeg":
-      break;
-    case "jpg":
-      break;
-    case "png":
-      break;
-    default:
-      return res.status(400).send({ message: "Invalid extension document type." });
-  }
-
-  const route = path.resolve(__dirname, "../../../public/upload/photos/", file.name);
-
-  file.mv(route, (err: any) => {
-    if (err) {
+router.post("/upload/photos", validateToken, async (req: Request, res: Response) => {
+  upload(req, res, function (err: any) {
+    try {
+      if (err instanceof multer.MulterError) {
+        console.log({ err });
+        // A Multer error occurred when uploading.
+        return res.status(500).send({ err });
+      } else if (err) {
+        console.log({ err });
+        return res.status(500).send({ err });
+        // An unknown error occurred when uploading.
+      }
+      const files = req.files;
+      const id = (req as any).user.id;
+      console.log("El archivo que se esta pasando es: ", { files });
+      console.log({ id });
+      return res.send("recieved your request!");
+    } catch (err) {
       console.log({ err });
-      return res.status(500).send(err);
     }
-    return res.send({ status: "success", route: route });
   });
 });
 
