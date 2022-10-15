a340210d39215a201ce66fd26960b5b8ad848e3b
Alejandro Martínez Luna <A01276785@tec.mx>
2022-10-15 15:07:17 -0500

    [API] [WEB] Removed trash code
    Co-authored-by: Naiztu <Naiztu@users.noreply.github.com>

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -75,74 +75,44 @@ const Home = (): JSX.Element => {
           </button>
         </div>
         <div>
-          <div className="flex row-span-1">
-            <div className="my-5 flex flex-col">
-              <h1 className="text-4xl font-semibold text-main-500">Convocatorias</h1>
-            </div>
-            <div className="mx-7 my-1 flex flex-col">
-              <Link to={"/app/newannouncement"} className="navBarLink">
-                <button className="bg-main-500 hover:bg-main-500/70 ease-in-out duration-500 font-bold text-white rounded-md py-2 px-2 text-sm mt-5">
-                  + Nueva Convocatoria
-                </button>
-              </Link>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse">
-            <label className="text-sm"> Selecciona Convocatoria</label>
-          </div>
-          <div className="flex flex-row-reverse">
-            <button
-              onClick={handleAllCitations}
-              className="bg-white hover:bg-gray-300/70 ease-in-out duration-500 font-bold text-gray-500 rounded-md py-2 px-2 text-sm shadow-sm shadow-slate-300/75 border border-b-slate-300 m-1"
-            >
-              Todas
-            </button>
-            <button
-              onClick={handleCitations}
-              className="bg-main-500 hover:bg-main-500/70 ease-in-out duration-500 font-bold text-white rounded-md py-2 px-2 text-sm shadow-sm shadow-slate-300/75 border border-b-slate-300 m-1"
-            >
-              Activas
-            </button>
-          </div>
-          <div>
-            {showCitation ? (
-              <>
-                <div className="flex flex-row flex-wrap w-full">
-                  {citations
-                    ?.filter((element: any) => element.end_date >= date)
-                    .map((filteredElement: any) => (
-                      <div className="flex flex-col basis-1/3" key={filteredElement.id}>
-                        <NoticeCard
-                          img={filteredElement.citation_description}
-                          name={filteredElement.citation_title}
-                          pdf={filteredElement.citation_document}
-                          date={filteredElement.end_date}
-                          id={filteredElement.id}
-                        />
-                      </div>
-                    ))}
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="flex flex-row flex-wrap">
-                  {citations?.map((element: any) => (
-                    <div className="flex flex-col basis-1/3" key={element.id}>
+          {showCitation ? (
+            <>
+              <div className="flex flex-row flex-wrap w-full">
+                {citations
+                  ?.filter((element: any) => element.end_date >= date)
+                  .map((filteredElement: any) => (
+                    <div className="flex flex-col basis-1/3" key={filteredElement.id}>
                       <NoticeCard
-                        pdf={element.citation_document}
-                        img={element.citation_description}
-                        name={element.citation_title}
-                        date={element.end_date}
-                        id={element.id}
+                        img={filteredElement.citation_description}
+                        name={filteredElement.citation_title}
+                        pdf={filteredElement.citation_document}
+                        date={filteredElement.end_date}
+                        id={filteredElement.id}
                       />
                     </div>
                   ))}
-                </div>
-              </>
-            )}
-          </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-row flex-wrap">
+                {citations?.map((element: any) => (
+                  <div className="flex flex-col basis-1/3" key={element.id}>
+                    <NoticeCard
+                      pdf={element.citation_document}
+                      img={element.citation_description}
+                      name={element.citation_title}
+                      date={element.end_date}
+                      id={element.id}
+                    />
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 };
