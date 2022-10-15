c01d0b77cfe98345708978ce56f6b2e2e25bdbba
Fermin Mendez <A01703366@itesm.mx>
2022-10-14 22:41:13 -0500

    [API] DocumentType mutation

--- api/source/graphql/mutations/DocumentTypeMutations.ts
+++ api/source/graphql/mutations/DocumentTypeMutations.ts
@@ -1,7 +1,7 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
 import { DocumentTypeType } from "../../types/DocumentTypeType";
-import { GraphQLError, GraphQLNonNull, GraphQLString } from "graphql";
+import { GraphQLError, GraphQLID, GraphQLNonNull, GraphQLString } from "graphql";
 import { v4 as uuid } from "uuid";
 import { db } from "../../database/database";
 import { DOCUMENT_TYPE_TABLE_NAME } from "../../database/utils/database_constants";
@@ -10,22 +10,45 @@ export default {
   createDocumentType: {
     type: DocumentTypeType,
     args: {
-      name: {
+      type_name: {
         type: GraphQLNonNull(GraphQLString),
       },
     },
-    resolve: async (_: any, { name }: any) => {
+    resolve: async (_: any, { type_name }: any) => {
       const id = uuid();
       await db(DOCUMENT_TYPE_TABLE_NAME)
         .insert({
           id,
-          name
+          type_name,
         })
         .catch((error: Error) => {
           console.error(error);
           throw new GraphQLError(error.name);
         });
 
+      const newDocumentTypeType = await db.select().from(DOCUMENT_TYPE_TABLE_NAME).where({ id });
+      return newDocumentTypeType[0];
+    },
+  },
+
+  updateDocumentType: {
+    type: DocumentTypeType,
+    args: {
+      id: {
+        type: GraphQLNonNull(GraphQLID),
+      },
+      type_name: {
+        type: GraphQLNonNull(GraphQLString),
+      },
+    },
+    resolve: async (_: any, { id, type_name }: any) => {
+      await db(DOCUMENT_TYPE_TABLE_NAME)
+        .update({ type_name })
+        .where({ id })
+        .catch((error: Error) => {
+          console.error(error);
+          throw new GraphQLError(error.name);
+        });
       const newDocumentTypeType = await db.select().from(DOCUMENT_TYPE_TABLE_NAME).where({ id });
       return newDocumentTypeType[0];
     },
