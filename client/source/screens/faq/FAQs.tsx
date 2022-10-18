c17dad76eb2eb4da7a38cd564ac997a5d1b07323
JordanaBM <A01707434@itesm.mx>
2022-10-18 14:26:05 -0500

    [CLIENT] Add chatbot implementation

--- client/source/screens/faq/FAQs.tsx
+++ client/source/screens/faq/FAQs.tsx
@@ -1,10 +1,14 @@
 // (c) Tecnologico de Monterrey 2022, rights reserved.
 
-import { FlatList, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { useLazyLoadQuery } from "react-relay";
 import { graphql } from "react-relay/hooks";
+import * as React from 'react';
+import { List } from 'react-native-paper';
+
 
 import { FAQsQuery, FAQsQuery$data } from "./__generated__/FAQsQuery.graphql";
+import { fontScale } from "nativewind";
 
 const FAQs = (): JSX.Element => {
   const data: FAQsQuery$data = useLazyLoadQuery<FAQsQuery>(
@@ -22,27 +26,47 @@ const FAQs = (): JSX.Element => {
     {},
   );
 
+  const [expanded, setExpanded] = React.useState(true);
+
+  const handlePress = () => setExpanded(!expanded);
+
   return (
     <>
       <SafeAreaView>
-        <FlatList
-          data={data?.faqs}
-          renderItem={({ item }) => (
-            <View className="p-2">
-              <View className="px-4">
-                <Text>Question:</Text>
-                <TouchableOpacity className="bg-blue-500 p-2 rounded-lg">
-                  <Text className="text-white text-lg text-center">{item.question}</Text>
-                </TouchableOpacity>
-              </View>
-              <View className="flex-row">
-                <Text>Answer:</Text>
-                <Text>{item.answer}</Text>
-              </View>
-            </View>
-          )}
-          keyExtractor={(item) => item.id}
-        />
+        <ScrollView>
+          <View className="p-5">
+            <List.Section>
+              {data.faqs.map((item, index) => (
+                <List.Accordion
+                  titleStyle={{
+                    textAlign: "justify",
+                    fontSize: 18,
+                  }}
+                  style={{ borderRadius: 20, 
+                    backgroundColor: '#6082B6',
+                     marginBottom:16, 
+                     justifyContent: "center", 
+                     alignItems: "center",}}
+                  titleNumberOfLines={10}
+                  key={index}
+                  title={<Text className="text-white">{item.question}</Text>}
+                  left={props => <List.Icon {...props} icon="frequently-asked-questions" color="white" />}>
+                  <List.Item
+                    titleStyle={{
+                      textAlign: "justify",
+                      fontSize: 15,
+                    }}
+                    left={props => <List.Icon {...props} icon="" color="black" />}
+                    right={props => <List.Icon {...props} icon="" color="black" />}
+                    style={{ backgroundColor: 'white', alignItems: "center", justifyContent: "center" }}
+                    titleNumberOfLines={30}
+                    key={item.id} title={<Text className="italic">{item.answer}</Text>} />
+                </List.Accordion>
+              ))}
+
+            </List.Section>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
