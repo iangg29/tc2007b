c3a2eed1b2dd5099afa41f887581c363c41e4bad
Fermin Mendez <A01703366@itesm.mx>
2022-10-18 14:07:23 -0500

    [API] Seeds data

--- api/source/database/seeds/14_applications_documents.ts
+++ api/source/database/seeds/14_applications_documents.ts
@@ -9,8 +9,16 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Inserts seed entries
   await knex(APPLICATION_DOCUMENTS_TABLE_NAME).insert([
-    { id: "1", application_id: "1", document_id: "2" },
-    { id: "2", application_id: "1", document_id: "4" },
-    { id: "3", application_id: "3", document_id: "2" },
+    { id: "501", application_id: "1", document_id: "1101" },
+    { id: "502", application_id: "1", document_id: "1102" },
+
+    { id: "511", application_id: "2", document_id: "1121" },
+    { id: "512", application_id: "2", document_id: "1122" },
+
+    { id: "521", application_id: "3", document_id: "1131" },
+    { id: "522", application_id: "3", document_id: "1132" },
+
+    { id: "508", application_id: "1", document_id: "4" },
+    { id: "509", application_id: "3", document_id: "2" },
   ]);
 }
